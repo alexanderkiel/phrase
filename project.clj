d2cd95f86c33ec019da87f15d7857833fded5ba4
Alexander Kiel <alexanderkiel@gmx.net>
2018-02-06 19:10:58 +0100

    Release v0.3-alpha3
    * Fix Issue with Global Symbols in Predicates #7, #8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject phrase "0.4-SNAPSHOT"
+(defproject phrase "0.3-alpha3"
   :description "Clojure(Script) library for phrasing spec problems."
   :url "https://github.com/alexanderkiel/phrase"
   :license {:name "Eclipse Public License"
